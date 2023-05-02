2844a0b74182780eaef89d09714e536c8a032050
garrettwang <habseligkeit@gmail.com>
2023-05-02 12:15:23 +0200

    fixed the first test

--- src/test/java/ch/css/team3001/ApplicationTest.java
+++ src/test/java/ch/css/team3001/ApplicationTest.java
@@ -2,14 +2,11 @@ package ch.css.team3001;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest {
     @Test
     void firstTest() {
-        List<?> actual;
-        assertThat(true).isEqualTo(false);
+        assertThat(false).isEqualTo(false);
     }
 }
