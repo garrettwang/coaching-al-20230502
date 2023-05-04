0e795d85689bc714ff95c418980799939b81666f
garrettwang <habseligkeit@gmail.com>
2023-05-04 22:21:24 +0200

    added validation for zero denominator

--- src/main/java/ch/css/team3001/Fraction.java
+++ src/main/java/ch/css/team3001/Fraction.java
@@ -12,24 +12,32 @@ class Fraction {
     }
 
     public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException();
+        }
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that) {
-        return new Fraction(this.numerator + that.numerator, that.denominator);
+        return new Fraction(this.numerator * that.denominator + this.denominator * that.numerator, this.denominator* that.denominator);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Fraction fraction = (Fraction) o;
-        return numerator == fraction.numerator && denominator == fraction.denominator;
+        Fraction that = (Fraction) o;
+        return numerator* that.denominator == that.numerator * denominator;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(numerator, denominator);
+    }
+
+    @Override
+    public String toString() {
+        return "%d/%d".formatted(numerator,denominator);
     }
 }
--- src/test/java/ch/css/team3001/FractionTest.java
+++ src/test/java/ch/css/team3001/FractionTest.java
@@ -2,35 +2,44 @@ package ch.css.team3001;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class FractionTest {
-    @Test
-    void zeroPlusZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assertions.assertThat(sum).isEqualTo(new Fraction(0));
+    public static Stream<Arguments> integerFractionTestdataProvider() {
+        return Stream.of(
+                Arguments.of(new Fraction(0), new Fraction(0),new Fraction(0)),
+                Arguments.of(new Fraction(6), new Fraction(0),new Fraction(6)),
+                Arguments.of(new Fraction(0), new Fraction(9),new Fraction(9)),
+                Arguments.of(new Fraction(11), new Fraction(8),new Fraction(19))
+        );
     }
 
-    @Test
-    void NotZeroPlusZero() {
-        Fraction sum = new Fraction(6).plus(new Fraction(0));
-        Assertions.assertThat(sum).isEqualTo(new Fraction(6));
+    @ParameterizedTest
+    @MethodSource(value = "integerFractionTestdataProvider")
+    void notZeroPlusNotZero(Fraction firstFraction, Fraction secondFraction, Fraction expectedFraction) {
+        Fraction sum = firstFraction.plus(secondFraction);
+        Assertions.assertThat(sum).isEqualTo(expectedFraction);
     }
 
-    @Test
-    void zeroPlusNotZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(9));
-        Assertions.assertThat(sum).isEqualTo(new Fraction(9));
-    }
-
-    @Test
-    void notZeroPlusNotZero() {
-        Fraction sum = new Fraction(11).plus(new Fraction(8));
-        Assertions.assertThat(sum).isEqualTo(new Fraction(19));
-    }
     @Test
     void onefifthPlusThreefifth() {
         Fraction sum = new Fraction(1, 5).plus(new Fraction(3,5));
         Assertions.assertThat(sum).isEqualTo(new Fraction(4,5));
     }
 
+    @Test
+    void coprimeFractions() {
+        Fraction sum = new Fraction(5, 7).plus(new Fraction(1,4));
+        Assertions.assertThat(sum).isEqualTo(new Fraction(27,28));
+    }
+
+    @Test
+    void zeroDenominator() {
+        Assertions.assertThatThrownBy(() -> new Fraction(1, 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
