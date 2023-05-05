24b58745e01b383f679ab01fa45353f3397ad264
garrettwang <habseligkeit@gmail.com>
2023-05-05 22:39:29 +0200

    extract FractionInput as a nested class

--- src/test/java/ch/css/team3001/FractionInputTest.java
+++ src/test/java/ch/css/team3001/FractionInputTest.java
@@ -3,32 +3,36 @@ package ch.css.team3001;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class FractionInputTest {
+    private final FractionParser fractionParser = new FractionParser();
+
     @Test
     void fractionWithSpace() {
-        Fraction actual = takeInput(" 5 / 2 ");
+        Fraction actual = fractionParser.takeInput(" 5 / 2 ");
         Assertions.assertThat(actual).isEqualTo(new Fraction(5,2));
     }
 
     @Test
     void fractionWithSpace3Over2() {
-        Fraction actual = takeInput(" 3 / 2 ");
+        Fraction actual = fractionParser.takeInput(" 3 / 2 ");
         Assertions.assertThat(actual).isEqualTo(new Fraction(3,2));
     }
 
-
     @Test
     void fractionWithSpace7Over4() {
-        Fraction actual = takeInput(" 7 /    4 ");
+        Fraction actual = fractionParser.takeInput(" 7 /    4 ");
         Assertions.assertThat(actual).isEqualTo(new Fraction(7,4));
     }
 
-    private Fraction takeInput(String fractionAsString) {
-        String[] splitedString = fractionAsString.split("/");
-        var numeratorString = splitedString[0];
-        var denominatorString = splitedString[1];
-        return new Fraction(Integer.parseInt(numeratorString.trim()), Integer.parseInt(denominatorString.trim()));
+    public class FractionParser {
+        public FractionParser() {
+        }
+
+        private Fraction takeInput(String fractionAsString) {
+            String[] splitedString = fractionAsString.split("/");
+            var numeratorString = splitedString[0];
+            var denominatorString = splitedString[1];
+            return new Fraction(Integer.parseInt(numeratorString.trim()), Integer.parseInt(denominatorString.trim()));
+        }
     }
 }
